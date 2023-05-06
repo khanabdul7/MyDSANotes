1477c9eec02e7aba87de4490a3529f5b901fee29
Abdul khan <abdulmatania@gmail.com>
2023-05-06 12:43:40 +0530

    removing extra files

--- Recursion.java
+++ Recursion.java
@@ -1,25 +1,25 @@
 public class Recursion {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         //calling methods
         int recursionResult = factorialWithRecursion(3);
         int result = factorialWithoutRecursion(3);
-        System.out.println("using factorialWithRecursion "+recursionResult);
-        System.out.println("using factorialWithoutRecursion "+result);
+        System.out.println("using factorialWithRecursion " + recursionResult);
+        System.out.println("using factorialWithoutRecursion " + result);
     }
 
     //with recursion
-    public static int factorialWithRecursion(int num){
-        if(num<=1)
+    public static int factorialWithRecursion(int num) {
+        if (num <= 1)
             return 1;
         else
-            return (num * factorialWithRecursion(num-1));
+            return (num * factorialWithRecursion(num - 1));
     }
 
     //without recursion
-    public static int factorialWithoutRecursion(int num){
+    public static int factorialWithoutRecursion(int num) {
         int factorial = 1;
-        for(int i=num; i>0; i--){
+        for (int i = num; i > 0; i--) {
             factorial = factorial * i;
         }
         return factorial;
